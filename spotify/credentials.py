c3e0040bc61f9af8a25f3142576ac47653e1d61f
PunyaKris <krishnatreypunya9211@gmail.com>
2026-03-06 23:59:33 +0530

    Redirect URI update

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = "4bdea01a7bc34ae1965b258929087cec"
 CLIENT_SECRET = "42c09d0b25d14dcf9ca722499cd3e2fa"
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+REDIRECT_URI = "https://music-party.onrender.com/spotify/redirect"
