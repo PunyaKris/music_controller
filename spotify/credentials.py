30589d80651aea35f0adbe1bcee5bc129884527b
PunyaKris <krishnatreypunya9211@gmail.com>
2026-03-07 19:46:34 +0530

    credential yy

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = "4bdea01a7bc34ae1965b258929087cec"
 CLIENT_SECRET = "42c09d0b25d14dcf9ca722499cd3e2fa"
-REDIRECT_URI = "https://music-partyy.onrender.com/spotify/redirect/"
+REDIRECT_URI = "https://music-partyy.onrender.com/spotify/redirect"
